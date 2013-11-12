0347f283a9c708584a878a33632dba71588ca753
Emil Apelgren <emilapelgren@hotmail.com>
2013-11-12 22:57:53 +0100

    Lägger till fungerande projekt.

---
+++ src/Coords.java
@@ -0,0 +1,24 @@
+
+
+public class Coords {
+        int x;
+        int y;
+        String s;
+        
+                public Coords(int x, int y){
+                        this.x = x;
+                        this.y = y;
+                }
+                
+                public Coords(String s){
+                        this.s = s;
+                }
+                
+                public int getX(){
+                        return x;
+                }
+                
+                public int getY(){
+                        return y;
+                }
+}
