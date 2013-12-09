f217b242740053a548bfbe530906f79fbf0436d2
Jonathan <jonathan.klingberg@gmail.com>
2013-12-09 12:45:44 +0100

    New Main-function

--- src/Main.java
+++ src/Main.java
@@ -2,58 +2,52 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 public class Main {
+	ServerSocket serverSocket = null;
+	private static int port = 21101;
+	private static int maxConnections = 0;
 
-        /**
-         * @param args
-         * @throws JSONException 
-         */
-        public static void main(String[] args) throws IOException, JSONException{
-        	ServerSocket serverSocket = null;
+	/**
+	 * @param args
+	 * @throws JSONException
+	 */
+	public static void main(String[] args) throws IOException, JSONException {
 
-//        	int x =3;
-//        	int y =3;
-        	StringBuilder sb = new StringBuilder("userString");
-    		sb.substring(1, sb.length());
-    		System.out.println(sb.toString());
-    		
-    		
+		int i = 0;
 
-    		
-//        	String userId = "hej";
-//        	String t;
-//        	DatabaseStarter dbs = new DatabaseStarter();
-//        	dbs.createConnection();
-//    		DatabaseHandler dbh = new DatabaseHandler();
-//    		t = dbh.setAndFetch(40, 30, "Johan");
-//    		String[] md = dbh.JsonToUser();
-//    		System.out.println(t);
-//    		System.out.println(md[1]);
-//       	System.out.println("update users set xcoord="+x+",ycoord="+y+
-//				" where userId="+"'"+userId+"'");
+		try {
+			ServerSocket listener = new ServerSocket(port);
 
-        	boolean listeningSocket = true;
-        	try {
-                System.out.println("Starting Socket thread...");
-        		serverSocket = new ServerSocket(21101);
-        		System.out.println("ServerSocket created, waiting for android device...");
-        	}catch (IOException e){
-        		System.err.println("Could not listen on port: 21101");
-        	}
-        	
-        	while(listeningSocket){
-        		Socket clientSocket = serverSocket.accept();
-        		MiniServer mini = new MiniServer(clientSocket);
-        		mini.start();
-        	}
-        	serverSocket.close();
-        		//ServerThread server = new ServerThread();
-        		//server.run();
-        }
+			while ((i++ < maxConnections) || (maxConnections == 0)) {
+				Socket clientSocket = listener.accept();
+				MiniServer mini = new MiniServer(clientSocket);
+				mini.start();
+			}
+		} catch (IOException ioe) {
+			System.out.println("IOException on socket listen: " + ioe);
+			ioe.printStackTrace();
+		}
+	}
+	// boolean listeningSocket = true;
+	// try {
+	// System.out.println("Starting Socket thread...");
+	// serverSocket = new ServerSocket(21101);
+	// System.out.println("ServerSocket created, waiting for android device...");
+	// }catch (IOException e){
+	// System.err.println("Could not listen on port: 21101");
+	// }
+	//
+	// while(listeningSocket){
+	// Socket clientSocket = serverSocket.accept();
+	// MiniServer mini = new MiniServer(clientSocket);
+	// mini.start();
+	// }
+	// serverSocket.close();
+	// //ServerThread server = new ServerThread();
+	// //server.run();
+	// }
 
 }
